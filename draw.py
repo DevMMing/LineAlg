d6d4ed972d8173f6bb3d40fb3afb7a32f39a6e5f
DevMMing <mming@stuy.edu>
2019-02-10 22:43:38 -0500

    done

--- draw.py
+++ draw.py
@@ -3,18 +3,58 @@ from display import *
 def draw_line( x0, y0, x1, y1, screen, color ):
     x=x0
     y=y0
-    A=abs(y1-y0)
-    B=abs(x1-x0)
-    if y0<y1:#oct 1 && oct 8
-            d=2A-B
-            while x <=x1:
+    A=y1-y0 
+    B=x1-x0
+    if B==0:
+        if(y<y1):
+            while(y<y1):
                 plot(screen,color,x,y)
-                if d > 0:
-                    y++
-                    d-=2B
-                x++
-                d+=2A
-    else if y0>y1:
-            pass
-    else:#dot
-            plot(screen,color, x0,y0)
+                y+=1
+        else:
+            while(y>y1):
+                plot(screen,color,x,y)
+                y-=1
+    else:
+        if x0<x1:#oct 1 && oct 2
+            slope=A/B
+            if slope<=1 and slope>=0:
+                d=2*A-B
+                while x <= x1:
+                    plot(screen,color,x,y)
+                    if d > 0:
+                        y+=1
+                        d-=2*B
+                    x+=1
+                    d+=2*A
+            elif slope >1:
+                d=A-2*B
+                while y <= y1:
+                    plot(screen,color,x,y)
+                    if d<0:
+                        x+=1
+                        d+=2*A
+                    y+=1
+                    d-=2*B
+            #oct 7 && oct 8
+            elif slope>=-1 and slope<0:
+                d=2*A+B
+                while x <= x1:
+                    plot(screen,color,x,y)
+                    if d<0:
+                        y-=1
+                        d+=2*B
+                    x+=1
+                    d+=2*A
+            elif slope <-1:
+                d=A+2*B
+                while y >= y1:
+                    plot(screen,color,x,y)
+                    if d > 0:
+                        x+=1
+                        d+=2*A
+                    y-=1
+                    d+=2*B
+        elif x0>x1:
+            draw_line(x1,y1,x0,y0,screen,color)
+        else:
+            draw_line(x1,y1,x0,y0,screen,color)
--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-
+draw_line(250,250,0,0,screen,color)
+draw_line(250,250,125,0,screen,color)
+draw_line(250,250,250,0, screen,color)
+draw_line(250,250,375,0,screen,color)
+draw_line(250,250,500,0, screen,color)
+draw_line(250,250,500,125,screen,color)
+draw_line(250,250,500,250,screen,color)
+draw_line(250,250,500,375,screen,color)
+draw_line(250,250,500,500, screen,color)
+draw_line(250,250,375,500,screen,color)
+draw_line(250,250,250,500, screen,color)
+draw_line(250,250,125,500,screen,color)
+draw_line(250,250,0,500, screen,color)
+draw_line(250,250,0,375,screen,color)
+draw_line(250,250,0,250, screen,color)
+draw_line(250,250,0,125,screen,color)
 display(screen)
 save_extension(screen, 'img.png')
